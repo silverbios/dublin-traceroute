af5ea7cb3367aea2eb5ae64e2a4cfbcef932e692
silverbios <aliasadollahi@aol.com>
2020-12-17 03:55:53 +0330

    optimizing CI build for macos

--- .travis/docker/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -exu
-
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push "${TRAVIS_REPO_SLUG}"
-if [ "${TRAVIS_BRANCH}" != "master" ]; then
-  docker tag "${TRAVIS_REPO_SLUG}" "${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}"
-  docker push "${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}"
-fi
